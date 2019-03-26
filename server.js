77cfd6fe08f7d4f367dcb66eb68b61da3627c7ed
Alan Jones <jonezzyboy@gmail.com>
2019-03-26 16:53:35 +0000

    Updated server.js
    Added in database connection

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const
 express = require('express'),
 app = express(),
 path = require('path'),
-routes = require(path.join(__dirname, 'server/routes.js'));
+routes = require(path.join(__dirname, 'server/routes.js')),
+mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://webres:webres@solacecluster-ihwta.mongodb.net/PITS?retryWrites=true', { useNewUrlParser: true });
+const db = mongoose.connection;
 
 app.use('/', express.static('client/dist'));
 
